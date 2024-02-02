298975a22e8fa7c0f23b6194ebb507a3db6ae3fe
gautam-dawar-99 <157568087+gautam-dawar-99@users.noreply.github.com>
2024-02-02 12:35:49 +0530

    Add files via upload

---
+++ source.js
@@ -0,0 +1,11 @@
+function data_get(){
+    var x=document.querySelector('#user').value;
+
+    
+    var y=document.querySelector('#passing').value;
+
+    console.log(x+"  "+y);
+
+    document.write(x+" "+y);
+
+}
